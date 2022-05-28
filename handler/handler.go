3507552ea85ba401a81a2c996039e254c4e48a5e
hojin <hojin@hojin.local>
2022-05-28 15:16:18 +0900

    fix handler func

--- handler/handler.go
+++ handler/handler.go
@@ -9,10 +9,10 @@ import (
 
 func ApplyHandler(e *echo.Echo) {
 	// 회원가입 API
-	e.POST("/api/signup", signUp)
+	e.POST("/api/signup", signUp())
 
 	// 로그인 API(현재는 테스트용)
-	e.POST("/api/signin", signIn)
+	e.POST("/api/signin", signIn())
 
 	// 목데이터로 테스트
 	e.GET("/api/getlist", healthCheck(), middleware.JWTWithConfig(middleware.JWTConfig{
--- handler/sign_in.go
+++ handler/sign_in.go
@@ -12,48 +12,50 @@ import (
 	"github.com/labstack/echo/v4"
 )
 
-func signIn(c echo.Context) error {
-	user := new(models.User)
+func signIn() echo.HandlerFunc {
+	return func(c echo.Context) error {
+		user := new(models.User)
 
-	if err := c.Bind(user); err != nil {
-		return c.JSON(http.StatusBadRequest, map[string]string{
-			"message": "bad request",
-		})
+		if err := c.Bind(user); err != nil {
+			return c.JSON(http.StatusBadRequest, map[string]string{
+				"message": "bad request",
+			})
+		}
+		inputpw := user.Password
+
+		db := db.Connect()
+		result := db.Find(user, "email=?", user.Email)
+
+		// 존재하지않는 아이디일 경우
+		if result.RowsAffected == 0 {
+			return echo.ErrBadRequest
+		}
+
+		res := helper.CheckPasswordHash(user.Password, inputpw)
+
+		// 비밀번호 검증에 실패한 경우
+		if !res {
+			return echo.ErrUnauthorized
+		}
+		// 토큰 발행
+		accessToken, err := helper.CreateJWT(user.Email)
+		if err != nil {
+			return echo.ErrInternalServerError
+		}
+
+		cookie := new(http.Cookie)
+		cookie.Name = "access-token"
+		cookie.Value = accessToken
+		cookie.HttpOnly = true
+		cookie.Expires = time.Now().Add(time.Hour * 24)
+
+		c.SetCookie(cookie)
+
+		if err := c.JSON(http.StatusOK, map[string]string{
+			"message": "Login Success",
+		}); err != nil {
+			return errors.Wrap(err, "signIn")
+		}
+		return nil
 	}
-	inputpw := user.Password
-
-	db := db.Connect()
-	result := db.Find(user, "email=?", user.Email)
-
-	// 존재하지않는 아이디일 경우
-	if result.RowsAffected == 0 {
-		return echo.ErrBadRequest
-	}
-
-	res := helper.CheckPasswordHash(user.Password, inputpw)
-
-	// 비밀번호 검증에 실패한 경우
-	if !res {
-		return echo.ErrUnauthorized
-	}
-	// 토큰 발행
-	accessToken, err := helper.CreateJWT(user.Email)
-	if err != nil {
-		return echo.ErrInternalServerError
-	}
-
-	cookie := new(http.Cookie)
-	cookie.Name = "access-token"
-	cookie.Value = accessToken
-	cookie.HttpOnly = true
-	cookie.Expires = time.Now().Add(time.Hour * 24)
-
-	c.SetCookie(cookie)
-
-	if err := c.JSON(http.StatusOK, map[string]string{
-		"message": "Login Success",
-	}); err != nil {
-		return errors.Wrap(err, "signIn")
-	}
-	return nil
 }
