8c61bce4589c4ae25d42624a750820c649315284
hojin <hojin@hojin.local>
2022-06-12 21:35:03 +0900

    deploy ci

--- main.go
+++ main.go
@@ -6,13 +6,6 @@ import (
 )
 
 func main() {
-
-	// godotenv는 로컬 개발환경에서 .env를 통해 환경변수를 읽어올 때 쓰는 모듈이다.
-	// 프로덕션 환경에서는 필요하지 않음.
-
-	// if err := godotenv.Load(".env"); err != nil {
-	// 	log.Fatal("Error loading .env file")
-	// }
 	e := echo.New()
 	handler.ApplyHandler(e)
 
